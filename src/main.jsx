f308e174aa72a27e14271bd76222ba636811dc64
StianL82 <stian.lilleng@gmail.com>
2025-02-27 17:39:53 +0100

    Updated App.jsx and main jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import GlobalStyle from "./styles/GlobalStyles";
+import GlobalStyles from "./styles/GlobalStyles";
 import theme from "./styles/theme";
+import App from "./App";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
